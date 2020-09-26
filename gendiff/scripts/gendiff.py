23d22ca803f085ef8bccc6168dde6ee72d778da4
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-09-26 12:17:32 +0300

    add dummy output

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.parse_args()
+    args = parser.parse_args()
+    print("First file: {}".format(args.first_file))
+    print("Second file: {}".format(args.second_file))
 
 
 if __name__ == '__main__':
