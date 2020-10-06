87d9b6192282215d6d88ad4e5e0c5bed0df4c914
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-10-06 20:37:23 +0300

    rename test files

--- tests/test_diff_functions.py
+++ tests/test_diff_functions.py
@@ -18,25 +18,25 @@ def test_generate_diff_with_plain_files(file_ext):
 
 
 @pytest.mark.parametrize('file_ext', ['.json', '.yml'])
-def test_generate_diff_with_recurse_files(file_ext):
+def test_generate_diff_with_recursive_files(file_ext):
     plain_file1_path = get_config_file_path(f'plain_file1{file_ext}')
-    recurse_file1_path = get_config_file_path(f'recurse_file1{file_ext}')
-    recurse_file2_path = get_config_file_path(f'recurse_file2{file_ext}')
+    rec_file1_path = get_config_file_path(f'rec_file1{file_ext}')
+    rec_file2_path = get_config_file_path(f'rec_file2{file_ext}')
 
-    diff_output1 = read_diff_output('recurse_file1_diff_recurse_file2')
-    assert generate_diff(recurse_file1_path, recurse_file2_path) == diff_output1
+    diff_output1 = read_diff_output('rec_file1_diff_rec_file2')
+    assert generate_diff(rec_file1_path, rec_file2_path) == diff_output1
 
-    diff_output2 = read_diff_output('recurse_file2_diff_recurse_file1')
-    assert generate_diff(recurse_file2_path, recurse_file1_path) == diff_output2
+    diff_output2 = read_diff_output('rec_file2_diff_rec_file1')
+    assert generate_diff(rec_file2_path, rec_file1_path) == diff_output2
 
-    diff_output3 = read_diff_output('plain_file1_diff_recurse_file1')
-    assert generate_diff(plain_file1_path, recurse_file1_path) == diff_output3
+    diff_output3 = read_diff_output('plain_file1_diff_rec_file1')
+    assert generate_diff(plain_file1_path, rec_file1_path) == diff_output3
 
 
 @pytest.mark.parametrize('file_ext', ['.json', '.yml'])
 def test_generate_diff_with_empty_files(file_ext):
     plain_file1_path = get_config_file_path(f'plain_file1{file_ext}')
-    recurse_file1_path = get_config_file_path(f'recurse_file1{file_ext}')
+    rec_file1_path = get_config_file_path(f'rec_file1{file_ext}')
     empty_file_path = get_config_file_path(f'empty{file_ext}')
 
     diff_output1 = read_diff_output('empty_diff_plain_file1')
@@ -48,11 +48,11 @@ def test_generate_diff_with_empty_files(file_ext):
     diff_output3 = read_diff_output('empty_diff_empty')
     assert generate_diff(empty_file_path, empty_file_path) == diff_output3
 
-    diff_output4 = read_diff_output('empty_diff_recurse_file1')
-    assert generate_diff(empty_file_path, recurse_file1_path) == diff_output4
+    diff_output4 = read_diff_output('empty_diff_rec_file1')
+    assert generate_diff(empty_file_path, rec_file1_path) == diff_output4
 
-    diff_output5 = read_diff_output('recurse_file1_diff_empty')
-    assert generate_diff(recurse_file1_path, empty_file_path) == diff_output5
+    diff_output5 = read_diff_output('rec_file1_diff_empty')
+    assert generate_diff(rec_file1_path, empty_file_path) == diff_output5
 
 
 def test_generate_diff_with_unsopported_file_type():
@@ -63,10 +63,10 @@ def test_generate_diff_with_unsopported_file_type():
 
 
 @pytest.mark.parametrize('file_ext', ['.json', '.yml'])
-def test_generate_diff_structure_with_recurse_files(file_ext):
-    config1 = load_config_file(get_config_file_path(f'recurse_file1{file_ext}'))
-    config2 = load_config_file(get_config_file_path(f'recurse_file2{file_ext}'))
-    diff_structure = read_diff_structure('recurse_file1_diff_recurse_file2')
+def test_generate_diff_structure_with_recursive_files(file_ext):
+    config1 = load_config_file(get_config_file_path(f'rec_file1{file_ext}'))
+    config2 = load_config_file(get_config_file_path(f'rec_file2{file_ext}'))
+    diff_structure = read_diff_structure('rec_file1_diff_rec_file2')
     assert generate_diff_structure(config1, config2) == diff_structure
 
 
