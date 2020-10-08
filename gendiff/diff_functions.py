d05886eabf88c793631b2cd06a6ac13007e538b6
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-10-08 11:01:19 +0300

    change structure of diff file

--- gendiff/diff_functions.py
+++ gendiff/diff_functions.py
@@ -4,83 +4,76 @@ from gendiff.file_loader import load_config_file
 def generate_diff(file1_path, file2_path):
     config1 = load_config_file(file1_path)
     config2 = load_config_file(file2_path)
-    diff = generate_diff_structure(config1, config2)
-    return ''.join(render_diff(diff))
+    structure = generate_diff_structure(config1, config2)
+    return render_diff_structure(structure)
 
 
-def render_diff(diff, cur_result=None, shift=0):
-    cur_result = cur_result or []
-    cur_result += ['{', '\n']
-    shift += 4
-
-    for key, item in sorted(diff.items()):
-        if item['status'] in {'removed', 'added', 'unchanged'}:
-            render_diff_item(
-                key, item['value'], item['status'], cur_result, shift
-            )
-        elif item['status'] == 'changed':
-            render_diff_item(key, item['old'], 'removed', cur_result, shift)
-            render_diff_item(key, item['new'], 'added', cur_result, shift)
-        elif item['status'] == 'children_changed':
-            cur_result += [' ' * shift, key, ': ']
-            render_diff(item['children'], cur_result, shift)
-
-    shift -= 4
-    cur_result += [' ' * shift, '}', '\n']
-    return cur_result
+NEW_LEVEL_SHIFT = 4
 
 
-def render_diff_item(key, value, status, cur_result, shift=0):
-    if status == 'removed':
-        prepend = '- '
-    elif status == 'added':
-        prepend = '+ '
-    elif status == 'unchanged' or status is None:
-        prepend = '  '
+def render_diff_structure(diff_structure, shift=0):
+    result = [render_open_braket()]
+    shift += NEW_LEVEL_SHIFT
 
-    cur_result += [' ' * (shift - 2), prepend, key, ': ']
+    for item in diff_structure:
+        if item['status'] == 'children_changed':
+            result.append(render_diff_item_key(item['key'], shift))
+            result.append(render_diff_structure(item['children'], shift))
+        else:
+            result.append(render_diff_item(
+                item['key'],
+                item['value'],
+                item['status'],
+                shift,
+            ))
+
+    result.append(render_closed_braket(shift - NEW_LEVEL_SHIFT))
+    return ''.join(result)
+
+
+def render_diff_item(key, value, status, shift=0):
+    prepend = get_diff_item_prepend(status)
+    result = [render_diff_item_key(key, shift - len(prepend), prepend)]
 
     if isinstance(value, dict):
-        cur_result += ['{', '\n']
-        for key, item in sorted(value.items()):
-            render_diff_item(key, item, None, cur_result, shift + 4)
-        cur_result += [' ' * shift, '}', '\n']
+        result.append(render_open_braket())
+        for k, v in value.items():
+            result.append(render_diff_item(k, v, None, shift + NEW_LEVEL_SHIFT))
+        result.append(render_closed_braket(shift))
     else:
-        cur_result += [to_diff_item_value(value), '\n']
+        result.append(render_diff_item_plain_value(value))
+
+    return ''.join(result)
 
 
-def generate_diff_structure(config1, config2, structure=None):
-    if structure is None:
-        structure = {}
-
-    config1_keys, config2_keys = set(config1), set(config2)
-
-    # Adding removed items
-    for key in config1_keys.difference(config2_keys):
-        structure[key] = {'status': 'removed', 'value': config1[key]}
-
-    # Adding added items
-    for key in config2_keys.difference(config1_keys):
-        structure[key] = {'status': 'added', 'value': config2[key]}
-
-    for key in config1_keys.intersection(config2_keys):
-        value1, value2 = config1[key], config2[key]
-
-        # Adding unchanged items
-        if value1 == value2:
-            structure[key] = {'status': 'unchanged', 'value': value1}
-        # Adding items with changed children
-        elif isinstance(value1, dict) and isinstance(value2, dict):
-            structure[key] = {'status': 'children_changed', 'children': {}}
-            generate_diff_structure(value1, value2, structure[key]['children'])
-        # Adding changed items
-        else:
-            structure[key] = {'status': 'changed', 'old': value1, 'new': value2}
-
-    return structure
+def render_open_braket(shift=0):
+    return ' ' * shift + '{\n'
 
 
-def to_diff_item_value(value):
+def render_closed_braket(shift=0):
+    return ' ' * shift + '}\n'
+
+
+def render_diff_item_key(key, shift=0, prepend=''):
+    return ' ' * shift + prepend + key + ': '
+
+
+def render_diff_item_plain_value(value):
+    return to_normalized_str(value) + '\n'
+
+
+def get_diff_item_prepend(item_status):
+    if item_status == 'removed':
+        prepend = '- '
+    elif item_status == 'added':
+        prepend = '+ '
+    elif item_status == 'unchanged' or item_status is None:
+        prepend = '  '
+
+    return prepend
+
+
+def to_normalized_str(value):
     if value is None:
         result = 'null'
     elif value is True:
@@ -91,3 +84,58 @@ def to_diff_item_value(value):
         result = str(value)
 
     return result
+
+
+def generate_diff_structure(config1, config2):
+    structure = []
+    config1_keys, config2_keys = set(config1), set(config2)
+
+    # Adding removed items
+    for key in sorted(config1_keys.difference(config2_keys)):
+        structure.append({
+            'key': key,
+            'value': config1[key],
+            'status': 'removed',
+        })
+
+    # Adding added items
+    for key in sorted(config2_keys.difference(config1_keys)):
+        structure.append({
+            'key': key,
+            'value': config2[key],
+            'status': 'added',
+        })
+
+    for key in sorted(config1_keys.intersection(config2_keys)):
+        value1, value2 = config1[key], config2[key]
+
+        # Adding unchanged items
+        if value1 == value2:
+            structure.append({
+                'key': key,
+                'value': value1,
+                'status': 'unchanged',
+            })
+        # Adding items with changed children
+        elif isinstance(value1, dict) and isinstance(value2, dict):
+            children = []
+            structure.append({
+                'key': key,
+                'children': children,
+                'status': 'children_changed',
+            })
+            children.extend(generate_diff_structure(value1, value2))
+        # Adding changed items
+        else:
+            structure.append({
+                'key': key,
+                'value': value1,
+                'status': 'removed',
+            })
+            structure.append({
+                'key': key,
+                'value': value2,
+                'status': 'added',
+            })
+
+    return structure
--- tests/test_diff_functions.py
+++ tests/test_diff_functions.py
@@ -10,11 +10,8 @@ def test_generate_diff_with_plain_files(file_ext):
     file1_path = get_config_file_path(f'plain_file1{file_ext}')
     file2_path = get_config_file_path(f'plain_file2{file_ext}')
 
-    diff_output1 = read_diff_output('plain_file1_diff_plain_file2')
-    assert generate_diff(file1_path, file2_path) == diff_output1
-
-    diff_output2 = read_diff_output('plain_file2_diff_plain_file1')
-    assert generate_diff(file2_path, file1_path) == diff_output2
+    diff_output = read_diff_output('plain_file1_diff_plain_file2')
+    assert generate_diff(file1_path, file2_path) == diff_output
 
 
 @pytest.mark.parametrize('file_ext', ['.json', '.yml'])
@@ -26,11 +23,8 @@ def test_generate_diff_with_recursive_files(file_ext):
     diff_output1 = read_diff_output('rec_file1_diff_rec_file2')
     assert generate_diff(rec_file1_path, rec_file2_path) == diff_output1
 
-    diff_output2 = read_diff_output('rec_file2_diff_rec_file1')
-    assert generate_diff(rec_file2_path, rec_file1_path) == diff_output2
-
-    diff_output3 = read_diff_output('plain_file1_diff_rec_file1')
-    assert generate_diff(plain_file1_path, rec_file1_path) == diff_output3
+    diff_output2 = read_diff_output('plain_file1_diff_rec_file1')
+    assert generate_diff(plain_file1_path, rec_file1_path) == diff_output2
 
 
 @pytest.mark.parametrize('file_ext', ['.json', '.yml'])
@@ -39,20 +33,14 @@ def test_generate_diff_with_empty_files(file_ext):
     rec_file1_path = get_config_file_path(f'rec_file1{file_ext}')
     empty_file_path = get_config_file_path(f'empty{file_ext}')
 
-    diff_output1 = read_diff_output('empty_diff_plain_file1')
-    assert generate_diff(empty_file_path, plain_file1_path) == diff_output1
+    diff_output1 = read_diff_output('empty_diff_empty')
+    assert generate_diff(empty_file_path, empty_file_path) == diff_output1
 
-    diff_output2 = read_diff_output('plain_file1_diff_empty')
-    assert generate_diff(plain_file1_path, empty_file_path) == diff_output2
+    diff_output2 = read_diff_output('empty_diff_plain_file1')
+    assert generate_diff(empty_file_path, plain_file1_path) == diff_output2
 
-    diff_output3 = read_diff_output('empty_diff_empty')
-    assert generate_diff(empty_file_path, empty_file_path) == diff_output3
-
-    diff_output4 = read_diff_output('empty_diff_rec_file1')
-    assert generate_diff(empty_file_path, rec_file1_path) == diff_output4
-
-    diff_output5 = read_diff_output('rec_file1_diff_empty')
-    assert generate_diff(rec_file1_path, empty_file_path) == diff_output5
+    diff_output3 = read_diff_output('empty_diff_rec_file1')
+    assert generate_diff(empty_file_path, rec_file1_path) == diff_output3
 
 
 def test_generate_diff_with_unsopported_file_type():
@@ -63,16 +51,16 @@ def test_generate_diff_with_unsopported_file_type():
 
 
 @pytest.mark.parametrize('file_ext', ['.json', '.yml'])
-def test_generate_diff_structure_with_recursive_files(file_ext):
-    config1 = load_config_file(get_config_file_path(f'rec_file1{file_ext}'))
-    config2 = load_config_file(get_config_file_path(f'rec_file2{file_ext}'))
-    diff_structure = read_diff_structure('rec_file1_diff_rec_file2')
-    assert generate_diff_structure(config1, config2) == diff_structure
-
-
-@pytest.mark.parametrize('file_ext', ['.json', '.yml'])
 def test_generate_diff_structure_with_plain_files(file_ext):
     config1 = load_config_file(get_config_file_path(f'plain_file1{file_ext}'))
     config2 = load_config_file(get_config_file_path(f'plain_file2{file_ext}'))
     diff_structure = read_diff_structure('plain_file1_diff_plain_file2')
+    assert generate_diff_structure(config1, config2) == diff_structure
+
+
+@pytest.mark.parametrize('file_ext', ['.json', '.yml'])
+def test_generate_diff_structure_with_recursive_files(file_ext):
+    config1 = load_config_file(get_config_file_path(f'rec_file1{file_ext}'))
+    config2 = load_config_file(get_config_file_path(f'rec_file2{file_ext}'))
+    diff_structure = read_diff_structure('rec_file1_diff_rec_file2')
     assert generate_diff_structure(config1, config2) == diff_structure
