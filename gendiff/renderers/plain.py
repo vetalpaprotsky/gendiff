967575b77324bc097f661a4c73e75576383eda76
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-10-23 23:28:50 +0300

    refactor render_diff fucntion for plain output

--- gendiff/renderers/plain.py
+++ gendiff/renderers/plain.py
@@ -1,34 +1,24 @@
 def render_diff(diff_structure, parent_keys=None):
     result = []
-    if parent_keys is None:
-        parent_keys = []
 
     for key, data in diff_structure.items():
-        key_with_parents = parent_keys + [key]
+        key_with_parents = (parent_keys or []) + [key]
+        key_path = '.'.join(key_with_parents)
         if data['status'] == 'children_updated':
             result.append(render_diff(data['children'], key_with_parents))
-        else:
-            result.append(_render_item(key_with_parents, data))
+        elif data['status'] == 'added':
+            value = _to_normalized_str(data['value'])
+            result.append(f'Propery {key_path} was added with value: {value}\n')
+        elif data['status'] == 'updated':
+            old = _to_normalized_str(data['old_value'])
+            new = _to_normalized_str(data['new_value'])
+            result.append(
+                f'Propery {key_path} was updated. From {old} to {new}\n'
+            )
+        elif data['status'] == 'removed':
+            result.append(f'Propery {key_path} was removed\n')
 
     return ''.join(result)
-
-
-def _render_item(key_with_parents, data):
-    key_path = '.'.join(key_with_parents)
-
-    if data['status'] == 'added':
-        value = _to_normalized_str(data['value'])
-        item_info = f'Propery {key_path} was added with value: {value}\n'
-    elif data['status'] == 'updated':
-        old = _to_normalized_str(data['old_value'])
-        new = _to_normalized_str(data['new_value'])
-        item_info = f'Propery {key_path} was updated. From {old} to {new}\n'
-    elif data['status'] == 'removed':
-        item_info = f'Propery {key_path} was removed\n'
-    else:
-        item_info = ''
-
-    return item_info
 
 
 def _to_normalized_str(value):
