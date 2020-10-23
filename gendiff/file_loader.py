f2d60b67ae1e242b6d7d81164160adde4c868439
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-10-23 15:04:37 +0300

    split login in load_file function into two functions

--- gendiff/file_loader.py
+++ gendiff/file_loader.py
@@ -6,16 +6,20 @@ import yaml
 def load_file(file_path):
     with open(file_path) as file:
         file_type = _get_file_type(file_path)
-        if file_type == 'json':
-            result = json.load(file)
-        elif file_type == 'yaml':
-            # If the file is empty, then the return value of load
-            # function is None. In that case an empty dict is assigned
-            # to the result variable.
-            result = yaml.load(file, Loader=yaml.Loader) or {}
-        else:
-            raise ValueError('Unsupported file type')
+        result = _parse_file(file, file_type)
+    return result
 
+
+def _parse_file(file, file_type):
+    if file_type == 'json':
+        result = json.load(file)
+    elif file_type == 'yaml':
+        # If the file is empty, then the return value of load
+        # function is None. In that case an empty dict is assigned
+        # to the result variable.
+        result = yaml.load(file, Loader=yaml.Loader) or {}
+    else:
+        raise ValueError('Unsupported file type')
     return result
 
 
@@ -23,5 +27,5 @@ def _get_file_type(file_path):
     _, file_ext = os.path.splitext(file_path)
     if file_ext == '.json':
         return 'json'
-    elif file_ext == '.yml':
+    elif file_ext == '.yml' or file_ext == '.yaml':
         return 'yaml'
