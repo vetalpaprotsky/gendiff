7e0e992e77e1f11ad5d75425ced2091d4bcca4e0
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-11-11 11:03:05 +0200

    simplify tests

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -1,47 +1,26 @@
 import pytest
-from tests.utils import read_diff_output, get_file_path
+from tests.utils import get_fixture_file_abs_path
 from gendiff.diff_generator import generate_diff
 
 
 @pytest.mark.parametrize(
-    'file1_name,file2_name,output_file_name,output_format',
+    'file1_path,file2_path,diff_file_path,output_format',
     [
-        ('file1.json', 'file2.json', 'stylish_file1_diff_file2.txt', 'stylish'),
-        ('file1.yml', 'empty.json', 'stylish_file1_diff_empty.txt', 'stylish'),
-        ('empty.yml', 'empty.yml', 'stylish_empty_diff_empty.txt', 'stylish'),
+        ('file1.json', 'file2.json', 'diff/stylish.txt', 'stylish'),
+        ('file1.yml', 'file2.yml', 'diff/stylish.txt', 'stylish'),
 
-        ('file1.json', 'file2.json', 'plain_file1_diff_file2.txt', 'plain'),
-        ('file1.yml', 'empty.json', 'plain_file1_diff_empty.txt', 'plain'),
-        ('empty.yml', 'empty.yml', 'plain_empty_diff_empty.txt', 'plain'),
+        ('file1.json', 'file2.json', 'diff/plain.txt', 'plain'),
+        ('file1.yml', 'file2.yml', 'diff/plain.txt', 'plain'),
 
-        ('file1.json', 'file2.json', 'json_file1_diff_file2.txt', 'json'),
-        ('file1.yml', 'empty.json', 'json_file1_diff_empty.txt', 'json'),
-        ('empty.yml', 'empty.yml', 'json_empty_diff_empty.txt', 'json'),
+        ('file1.json', 'file2.json', 'diff/json.txt', 'json'),
+        ('file1.yml', 'file2.yml', 'diff/json.txt', 'json'),
     ]
 )
-def test_generate_diff_with_different_file_formats_and_output_formats(
-    file1_name, file2_name, output_file_name, output_format
+def test_generate_diff(
+    file1_path, file2_path, diff_file_path, output_format
 ):
-    file1_path = get_file_path(file1_name)
-    file2_path = get_file_path(file2_name)
-
-    diff_output = read_diff_output(output_file_name)
-    assert generate_diff(file1_path, file2_path, output_format) == diff_output
-
-
-def test_generate_diff_with_unsupported_file_format():
-    file_path = get_file_path('empty.xml')
-
-    with pytest.raises(ValueError) as error:
-        generate_diff(file_path, file_path, 'plain')
-
-    assert str(error.value) == 'Unsupported file type'
-
-
-def test_generate_diff_with_unsupported_output_format():
-    file_path = get_file_path('empty.json')
-
-    with pytest.raises(ValueError) as error:
-        generate_diff(file_path, file_path, 'unexisting')
-
-    assert str(error.value) == 'Invalid output format'
+    file1_abs_path = get_fixture_file_abs_path(file1_path)
+    file2_abs_path = get_fixture_file_abs_path(file2_path)
+    with open(get_fixture_file_abs_path(diff_file_path)) as file:
+        diff = file.read()
+    assert generate_diff(file1_abs_path, file2_abs_path, output_format) == diff
--- tests/utils.py
+++ tests/utils.py
@@ -1,28 +1,5 @@
 import os
-import json
 
 
-def read_diff_output(file_name):
-    dir_path = os.path.join(
-        os.path.dirname(__file__),
-        'fixtures',
-        'diff_outputs',
-    )
-    file_path = os.path.join(dir_path, file_name)
-    with open(file_path) as file:
-        return file.read()
-
-
-def load_diff_tree(file_name):
-    dir_path = os.path.join(
-        os.path.dirname(__file__),
-        'fixtures',
-        'diff_trees'
-    )
-    file_path = os.path.join(dir_path, file_name)
-    with open(file_path) as file:
-        return json.load(file)
-
-
-def get_file_path(file_name):
+def get_fixture_file_abs_path(file_name):
     return os.path.join(os.path.dirname(__file__), 'fixtures', file_name)
