04ccd07251c67c7dff154ba5b60b3cc23f46cf04
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-11-24 11:57:07 +0200

    make better console help info

--- gendiff/args_parser.py
+++ gendiff/args_parser.py
@@ -2,13 +2,17 @@ import argparse
 
 
 def get_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    default_format = 'stylish'
     parser.add_argument(
         '-f', '--format',
         type=str,
-        help='set format of output',
-        default='stylish',
+        metavar='format',
+        help=f'output format (default: {default_format}, other: json, plain)',
+        default=default_format,
     )
     return parser
